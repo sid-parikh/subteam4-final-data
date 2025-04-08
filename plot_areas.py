23a47f46f5ecdfbe56bf5f1f45c204003015fe11
Sid Parikh <parikhsid16@gmail.com>
2025-04-08 18:09:14 -0400

    add plots

--- plot_areas.py
+++ plot_areas.py
@@ -2,7 +2,8 @@
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from collections import defaultdict
+from shapely.geometry import Point
 places = [
     (22052, 'Decatur'),
     (56112, 'North Decatur'),
@@ -21,7 +22,21 @@ place_geos = [
     for place_id, place_name in places
 ]
 
+stops_df = pd.read_csv('data/google_transit/stops.txt')
+
+places = defaultdict(list)
+
+for index, row in stops_df.iterrows():
+    stop_lon = row['stop_lon']
+    stop_lat = row['stop_lat']
+    for (place, name) in place_geos:
+        if any(place.geometry.contains(Point(stop_lon, stop_lat))):
+            places[name].append((stop_lon, stop_lat))
+
 for (place, name) in place_geos:
     place.plot()
-    plt.title(name)
+    for (lon, lat) in places[name]:
+        plt.scatter(lon, lat, color='black', s=5)
+    plt.title(f'{name} Transit Stops')
+    plt.savefig(f'data/places/{name}.png')
 plt.show()
